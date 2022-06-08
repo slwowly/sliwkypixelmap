227b9b6c1bc0026234312592c193b87ef7db3900
slwowly <68538475+slwowly@users.noreply.github.com>
2022-06-08 17:58:54 +0700

    Add files via upload

--- minimap.user.js
+++ minimap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         sliwky Minimap
 // @namespace    http://tampermonkey.net/
-// @version      1.09
+// @version      1.1
 // @description  sliwky Minimap
 // @author       IlyaBOT
 // @match        https://pixelplanet.fun/*
